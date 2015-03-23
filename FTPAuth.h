33cd0b4b7da74bb481ae81ef29dc0dfacf98440d
cora48 <cora48@test123.ru>
2015-03-23 16:54:40 +0300

    Minor refactorings

--- FTPAuth.h
+++ FTPAuth.h
@@ -10,10 +10,10 @@
 class FTPA {
     private:
     static bool checkOutput(const string *buffer);
-    static lopaStr _FTPBrute(const char *ip, const int port, PathStr *ps);
+    static lopaStr FTPBrute(const char *ip, const int port, PathStr *ps);
 
     public:
-    static lopaStr _FTPLobby(const char *ip, const int port, PathStr *ps);
+    static lopaStr FTPLobby(const char *ip, const int port, PathStr *ps);
 };
 
 #endif // FTPAUTH_H

73c24d990e3f40c10586d9463475c0fa8fb78124
cora32 <cora32@bk.ru>
2016-01-07 04:00:48 +0300

    lpcwstr fix

--- nesca_3.cpp
+++ nesca_3.cpp
@@ -3131,7 +3131,7 @@ QString GetColorCode(int mode, QString str)
 
 #if defined(WIN32) || defined(_WIN32) || defined(__WIN32) && !defined(__CYGWIN__)
 void enableHikvisionSupport(){
-	HINSTANCE hGetProcIDDLL = LoadLibrary(L".\\HCNetSDK.dll");
+	HINSTANCE hGetProcIDDLL = LoadLibraryW((LPCWSTR)L".\\HCNetSDK.dll");
 
 	if (!hGetProcIDDLL) {
 		HikVis::isInitialized = false;
